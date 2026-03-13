996affb97cfb1eeb166c595eeccc49d2d818c3c7
ELBOUKBIRI MOHAMMED AMINE <amine@ELBOUKBIRIs-MacBook-Pro.local>
2026-03-13 22:56:24 +0000

    first push

---
+++ src/main/java/ma/fst/projet_full_stack_1/ProjetFullStack1Application.java
@@ -0,0 +1,13 @@
+package ma.fst.projet_full_stack_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetFullStack1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetFullStack1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ma/fst/projet_full_stack_1/ProjetFullStack1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.fst.projet_full_stack_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetFullStack1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
